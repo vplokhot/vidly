7f01acf9bf4c104acdff9370df4c34c82670a7b2
vplokhot <vplokhotniouk@gmail.com>
2019-08-18 22:58:08 -0700

    fetch movies from fake service + delete & reset functionality

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,19 +6,83 @@ class Movies extends Component {
     movies: getMovies()
   };
 
-  listMovies = () => {
-    console.log(this.state.movies);
+  handleMovieDelete = movieId => {
+    const updatedMovies = this.state.movies.filter(
+      movie => movie._id !== movieId
+    );
+    this.setState({
+      movies: updatedMovies
+    });
+  };
+
+  resetMovies = () => {
+    this.setState({ movies: getMovies() });
+  };
+
+  renderMovies = () => {
+    if (this.state.movies.length === 0)
+      return (
+        <div>
+          "No movies available."
+          <button
+            className="btn btn-info"
+            onClick={() => {
+              this.resetMovies();
+            }}
+          >
+            Reset
+          </button>
+        </div>
+      );
+
+    return (
+      <div className="container">
+        <p>Showing {this.state.movies.length} available movies.</p>
+        <div className="movieList">
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Genre</th>
+                <th scope="col">Stock</th>
+                <th scope="col">Rate</th>
+                {/* empty table header for visual consistency; 
+                    table margins are uneven without it
+                */}
+                <th scope="col" />
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.movies.map(movie => (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        this.handleMovieDelete(movie._id);
+                      }}
+                      className="btn btn-danger"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
   };
 
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>Movies</h1>
-        <ul>
-          {this.state.movies.map(movie => (
-            <li key={movie._id}>{movie.title}</li>
-          ))}
-        </ul>
+        {this.renderMovies()}
       </div>
     );
   }
